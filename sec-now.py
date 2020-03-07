bc622ecb67e0dbdc657f330d7d3ad4fc1362f39d
Aashiq2805 <49115233+Aashiq2805@users.noreply.github.com>
2020-03-07 08:37:21 +0530

    Update sec-now.py

--- sec-now.py
+++ sec-now.py
@@ -4,7 +4,7 @@ import time
 
 
 #github access token being defined here
-ACCESS_TOKEN = '3c483466b20325641643dcada3aad4c6528ca24c'
+ACCESS_TOKEN = 'bb0a9604734003c2d96515a40f4428958c29c814'
 #twitter consumer tokens being defined here
 auth = tweepy.OAuthHandler("IUK4LzRRBjNZSPse0Yrh0ASzs", "R7wgP61StMDNKtSN71hWTj7ROjIa15m8643bfm9Cy8XsUG6BSR")
 #twitter access tokens being defined here
@@ -22,15 +22,13 @@ def search_github(keywords):
     for repo in result:
         #posting a tweet of security project over to twitter 
         api.update_status(repo.clone_url)
-
         #..........................................................................................................
         #below i have commented a line to print all repos gathered. used this when the 3 steps were separate code .
         #Now integerated it and the below line can be used for debugging                                          .
         #print(repo.clone_url)                                                                                    .                                                                                    
         #..........................................................................................................
-        
         #sleeping for 6 hours before posting again
-        #time.sleep(21600)
+        time.sleep(21600)
 
 if __name__ == '__main__':
     keywords = str(input('Enter terms to search in quotes: '))
