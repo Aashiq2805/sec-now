d241c3d0e2083adc525b5f2adcc0eb25be8ff4b4
Aashiq2805 <49115233+Aashiq2805@users.noreply.github.com>
2020-03-07 07:51:41 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+hey,
+
+above is the python file to search for trending repositories through github based on the topic of your preference and post it to twitter
+
+written in python3 it needs two packages to run successfully
+
+these packages are:
+1. tweepy
+2. PyGithub
+
+to install them :
+
+pip install PyGithub
+pip install tweepy
+
+post this we can run the python application to see the output in Lucy's twitter page
---
+++ sec-now.py
@@ -0,0 +1,38 @@
+from github import Github
+import tweepy
+import time
+
+
+#github access token being defined here
+ACCESS_TOKEN = '25f79a15f32e4128886ab91a2d91759900803558 '
+#twitter consumer tokens being defined here
+auth = tweepy.OAuthHandler("IUK4LzRRBjNZSPse0Yrh0ASzs", "R7wgP61StMDNKtSN71hWTj7ROjIa15m8643bfm9Cy8XsUG6BSR")
+#twitter access tokens being defined here
+auth.set_access_token("1226323598144458754-gAWJFxK7fzoioaPomxaUS7PemHhDIl", "MtMZOPHtFyTmAu14zwRRrgJNUZx8gDZUuItwXIz8VFMHv")
+#twitter api call being set here
+api = tweepy.API(auth)
+#github api call being set here
+g = Github(ACCESS_TOKEN)
+
+#function to search github
+def search_github(keywords):
+    query = '+'.join(keywords) + '+in:readme+in:description'
+    result = g.search_repositories(query, 'stars', 'desc')
+
+    for repo in result:
+        #posting a tweet of security project over to twitter 
+        api.update_status(repo.clone_url)
+
+        #..........................................................................................................
+        #below i have commented a line to print all repos gathered. used this when the 3 steps were separate code .
+        #Now integerated it and the below line can be used for debugging                                          .
+        #print(repo.clone_url)                                                                                    .                                                                                    .
+        #..........................................................................................................
+        
+        #sleeping for 30 minutes before posting again
+        time.sleep(86400)
+
+if __name__ == '__main__':
+    keywords = str(input('Enter terms to search in quotes: '))
+    keywords = [keyword.strip() for keyword in keywords.split(',')]
+    search_github(keywords)
