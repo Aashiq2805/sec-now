c3fbad6cc8cba395883524aa0b3c4bbca0047b05
Aashiq2805 <49115233+Aashiq2805@users.noreply.github.com>
2020-04-01 09:51:50 +0530

    Add files via upload

---
+++ twitter_worldwide_trending_hashtags.py
@@ -0,0 +1,20 @@
+import tweepy
+consumer_key = 'secret'
+consumer_secret = 'secret'
+access_token = 'secret'
+access_token_secret = 'secret'
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+trends1 = api.trends_place(1)#from the end of our code
+# trends1 is a list with only one element in it, which is a 
+# dict which we'll put in data.
+data = trends1[0] 
+# grab the trends
+trends = data['trends']
+# grab the name from each trend
+names = [trend['name'] for trend in trends]
+# put all the names together with a ' ' separating them
+trendsName = ' '.join(names)
+print(trendsName)
